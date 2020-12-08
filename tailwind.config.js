a2e3e347231f754c69b440cd127bb60b7dcfc230
Bryan M <allen.bry95@gmail.com>
2020-12-08 16:51:21 +0100

    style: configuring customized background colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@ module.exports = {
   purge: [ './src/**/*.js', './public/index.html' ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundColor: (theme) => ({
+      violet: {
+        pale: 'hsl(276, 100%, 81%)',
+        moderate: 'hsl(276, 55%, 52%)',
+        desaturated: {
+          dark: 'hsl(271, 15%, 43%)',
+          verydark: 'hsl(271, 36%, 24%)',
+        },
+      },
+      grayish: {
+        blue: 'hsl(206, 6%, 79%)',
+        light: {
+          violet: 'hsl(270, 20%, 96%)',
+        },
+        dark: {
+          violet: 'hsl(270, 7%, 64%)',
+        },
+      },
+    }),
     extend: {},
   },
   variants: {
