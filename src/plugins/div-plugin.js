0b6dbe36efa475ff0cd77f24d22ff45930fc944a
Bryan M <allen.bry95@gmail.com>
2020-12-08 18:03:37 +0100

    style: Decorative divs plugin added

---
+++ src/plugins/div-plugin.js
@@ -0,0 +1,10 @@
+module.exports = ({ addComponents }) => {
+  const divs = {
+    '.decorative': {
+      height: '695px',
+      width: '327px',
+      borderRadius: '322px',
+    },
+  };
+  addComponents(divs);
+};
