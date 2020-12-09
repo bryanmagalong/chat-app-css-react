98fbda128686d0a7dde4d5d09e760acca5180360
Bryan M <allen.bry95@gmail.com>
2020-12-09 22:32:28 +0100

    feat: add decorative divs in app background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import ContentWrapper from './components/ContentWrapper';
 
-function App() {
-  return (
-    <div className="container bg-very-light-grayish">
-      Main container
-      <div className="decorative bg-gradient-to-t from-light-violet to-light-magenta" />
-      <div className="decorative bg-light-grayish-violet" />
-    </div>
-  );
-}
+const App = () => (
+  <div className="container relative bg-very-light-grayish overflow-hidden">
+    <div className="absolute -left-40 -top-60 decorative bg-gradient-to-t from-light-violet to-light-magenta" />
+    <div className="absolute decorative bg-light-grayish-violet -right-40 -bottom-80" />
+    <ContentWrapper>
+      <div className="relative">
+        <div className="decorative bg-pale-violet mx-auto">Hello</div>
+      </div>
+    </ContentWrapper>
+  </div>
+);
 
 export default App;
