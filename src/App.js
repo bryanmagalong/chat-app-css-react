f8fadb97252f82e0b09010555198af1e13a9b0f2
Bryan M <allen.bry95@gmail.com>
2020-12-09 22:38:06 +0100

    feat(App): add 100vh minimum height

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ContentWrapper from './components/ContentWrapper';
 
 const App = () => (
-  <div className="container relative bg-very-light-grayish overflow-hidden">
+  <div className="container relative min-h-screen  overflow-hidden bg-very-light-grayish  ">
     <div className="absolute -left-40 -top-60 decorative bg-gradient-to-t from-light-violet to-light-magenta" />
     <div className="absolute decorative bg-light-grayish-violet -right-40 -bottom-80" />
     <ContentWrapper>
-      <div className="relative">
+      {/* <div className="relative">
         <div className="decorative bg-pale-violet mx-auto">Hello</div>
-      </div>
+      </div> */}
     </ContentWrapper>
   </div>
 );
