27ac301bd8e8be07823200f8e786161d164d254f
Bryan M <allen.bry95@gmail.com>
2020-12-11 23:18:16 +0100

    feat(MobileFrame): add borders, background color and box-shadow

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ContentWrapper from './components/ContentWrapper';
+import MobilePreview from './components/MobilePreview';
 
 const App = () => (
-  <div className="container relative min-h-screen  overflow-hidden bg-very-light-grayish  ">
+  <div className="container relative min-h-screen  overflow-hidden bg-very-light-grayish">
     <div className="absolute -left-36 -top-60 decorative bg-gradient-to-t from-light-violet to-light-magenta" />
     <div className="absolute decorative bg-light-grayish-violet -right-36 -bottom-80" />
     <ContentWrapper>
       {/* <div className="relative decorative mx-auto bg-pale-violet">Hello</div> */}
+      <MobilePreview />
     </ContentWrapper>
   </div>
 );
