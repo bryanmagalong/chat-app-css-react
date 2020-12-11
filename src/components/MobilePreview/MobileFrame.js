3b3dbcc73d89ca3c6ac6b25df54cf822a6be48c6
Bryan M <allen.bry95@gmail.com>
2020-12-11 22:23:04 +0100

    feat: add MobilePreview and MobileDevice components

---
+++ src/components/MobilePreview/MobileFrame.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MobileFrame = ({ children }) => (
+  <div className="relative mx-auto">{children}</div>
+);
+
+export default MobileFrame;
---
+++ src/components/MobilePreview/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import MobileFrame from './MobileFrame';
+
+const MobilePreview = () => <MobileFrame>MobilePreview Component</MobileFrame>;
+
+export default MobilePreview;
