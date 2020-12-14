67b62158cb88f8817a12568e38245ec39627e54a
Bryan M <allen.bry95@gmail.com>
2020-12-14 18:35:40 +0100

    feat(TextHeader): add mt-text-container class

--- src/components/TextHeader/index.js
+++ src/components/TextHeader/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TextHeader = ({ children }) => (
-  <div className="relative container w-72 mx-auto max-w-text-header text-title-header">
+  <div className="relative container mt-text-header w-72 mx-auto max-w-text-header text-title-header">
     {children}
   </div>
 );
