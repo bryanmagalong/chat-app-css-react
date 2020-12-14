e4a21b19a14ff82e39f9cb0305d752104d056c10
Bryan M <allen.bry95@gmail.com>
2020-12-14 18:14:59 +0100

    feat(TextHeader): add relative position

--- src/components/TextHeader/index.js
+++ src/components/TextHeader/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const TextHeader = ({ children }) => (
-  <div className="container w-72 mx-auto max-w-text-header">{children}</div>
+  <div className="relative container w-72 mx-auto max-w-text-header text-title-header">
+    {children}
+  </div>
 );
 
 export default TextHeader;
