629a165202b376bcd83eb843d5635e19bc066a7f
Bryan M <allen.bry95@gmail.com>
2020-12-21 15:40:04 +0100

    feat(MobileHeader): add Avatar display

--- src/components/MobileHeader/index.js
+++ src/components/MobileHeader/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Avatar from './Avatar';
+import avatar from './../../assets/images/avatar.jpg';
 
-const MobileHeader = ({ children }) => (
-  <div className="relative h-mfheader bg-gradient-to-r from-light-violet to-light-magenta rounded-b-md shadow-md">
+const MobileHeader = () => (
+  <div className="relative h-mfheader bg-gradient-to-r from-light-violet to-light-magenta rounded-b-md shadow-md px-5 pb-3">
     <div className="flex flex-row justify-center">
-      <div className="absolute -top-3 h-7 w-36 bg-white rounded-full" />
+      <div className="absolute -top-3 h-7 w-32 bg-white rounded-full" />
     </div>
-    {children}
+    <div className="container h-full flex items-end">
+      <Avatar
+        src={avatar}
+        alt="avatar"
+        name="Samuel Green"
+        status="Available to Walk"
+      />
+    </div>
   </div>
 );
 
