48826cf95b9b22809013e3e5c70204a0090e4d78
Bryan M <allen.bry95@gmail.com>
2020-12-29 17:13:16 +0100

    feat: add ChatSection and SubmitInput component

---
+++ src/components/MobilePreview/ChatSection.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ChatSection = ({ children }) => <div>{children}</div>;
+
+export default ChatSection;
---
+++ src/components/MobilePreview/SubmitInput.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SubmitInput = ({ children }) => <div>Submit input</div>;
+
+export default SubmitInput;
