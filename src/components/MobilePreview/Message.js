18a868f7ce209a2080844cfae42849b70f634354
Bryan M <allen.bry95@gmail.com>
2021-01-05 21:36:46 +0100

    feat(Message): add box shadow to right positioned messages

--- src/components/MobilePreview/Message.js
+++ src/components/MobilePreview/Message.js
@@ -6,7 +6,7 @@ const Message = (props) => (
     className={`max-w-message min-h-message-y mb-message-bottom py-message-padding-y px-message-padding-x text-message ${props.position ===
     'left'
       ? ' text-moderate-violet bg-very-light-violet rounded-message-left'
-      : 'text-dark-desaturated-violet bg-white rounded-message-right self-end'}`}
+      : 'text-dark-desaturated-violet bg-white rounded-message-right self-end shadow-sm'}`}
   >
     {props.children}
   </div>
