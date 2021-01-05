bba273094464cc4aac30ebb5e40a96a2820ff648
Bryan M <allen.bry95@gmail.com>
2021-01-05 16:47:31 +0100

    feat(ChatSection): add flex and flex-col classes

--- src/components/MobilePreview/ChatSection.js
+++ src/components/MobilePreview/ChatSection.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ChatSection = ({ children }) => (
-  <div className="h-full px-2 py-3">{children}</div>
+  <div className="h-full px-2 py-3 flex flex-col">{children}</div>
 );
 
 export default ChatSection;
