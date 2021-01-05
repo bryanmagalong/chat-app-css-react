31811c961cac964ef5d158ff26a8cda71ff02384
Bryan M <allen.bry95@gmail.com>
2021-01-05 12:10:32 +0100

    feat(SubmitInput): add styles, button and FontAwesomeIcon

--- src/components/MobilePreview/SubmitInput.js
+++ src/components/MobilePreview/SubmitInput.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const SubmitInput = ({ children }) => (
-  <div className="px-2 pb-2">Submit input</div>
+  <div className="px-2 pb-2">
+    <div className="flex justify-between items-center w-submit-x h-submit-y rounded-submit bg-white overflow-hidden p-submit-button">
+      <input
+        className="text-submit ml-placeholder"
+        type="text"
+        placeholder="Type your message..."
+        disabled
+      />
+      <div className="w-6 h-6 rounded-full bg-very-dark-desaturated-violet text-center text-white">
+        <button className="w-full" disabled>
+          <FontAwesomeIcon icon={faChevronRight} size="sm" />
+        </button>
+      </div>
+    </div>
+  </div>
 );
 
 export default SubmitInput;
