efc48f068ee5097c02e68849fb1788895b1793b6
Bryan M <allen.bry95@gmail.com>
2021-01-05 16:49:44 +0100

    feat(MobilePreview): add Message components on display

--- src/components/MobilePreview/index.js
+++ src/components/MobilePreview/index.js
@@ -4,15 +4,26 @@ import MobileFrame from './MobileFrame';
 import MobileHeader from '../../components/MobileHeader';
 import ChatSection from './ChatSection';
 import SubmitInput from './SubmitInput';
+import Message from './Message';
 
 const MobilePreview = () => (
   <MobileFrame>
     <MobileHeader />
     <ChatSection>
-      <p>Message</p>
-      <p>Message</p>
-      <p>Message</p>
-      <p>Message</p>
+      <Message position="left">
+        That sounds great. I’d be happy to discuss more.
+      </Message>
+      <Message position="left">
+        Could you send over some pictures of your dog, please?
+      </Message>
+      <Message position="right">
+        Here a few pictures. She's a happy girl!
+      </Message>
+      <Message position="right">Can you make it?</Message>
+      <Message position="left">
+        She looks so happy! The time we discussed works. How long shall i take
+        her out for?
+      </Message>
     </ChatSection>
     <SubmitInput />
   </MobileFrame>
