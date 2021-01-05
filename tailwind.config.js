b405752df88ab753b3fa1af22f7c275385284d52
Bryan M <allen.bry95@gmail.com>
2021-01-05 12:08:12 +0100

    chore(tailwind config): add custom variables for SubmitInput and MobileFrame components

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,15 +11,20 @@ module.exports = {
         // content gap
         content: '10.313em',
         // height
+        'submit-y': '2.125rem',
         frame: '30rem',
         mfheader: '4.5rem', // mobile frame header
         'decorative-y': '43.438rem',
         'decorative-y-lg': '63.625rem',
         // width
+        'frame-x': '16.625rem',
         'decorative-x': '20.438rem',
         'decorative-x-lg': '30.563rem',
+        'submit-x': '14.250rem',
         // position placement
         'lg-x': '30rem',
+
+        'submit-button': '0.167rem',
       },
       minWidth: {
         'text-header-lg': '24.938rem',
@@ -33,16 +38,19 @@ module.exports = {
       borderRadius: {
         frame: '2rem',
         decorative: '20.125em',
+        submit: '1.063rem',
       },
       fontSize: {
         'title-header': '2.375em',
         'content-header': '0.938rem',
         'avatar-name': '0.7rem',
         'avatar-status': '0.45rem',
+        submit: '0.563rem',
       },
       margin: {
         'text-header': '3.375rem',
         'content-header': '1.313rem',
+        placeholder: '1.188rem',
       },
       colors: {
         pale: {
